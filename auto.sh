8fdf01bc05ee492fb230c17cbe086af8c6a6de35
showzyl <showzyl520@gmail.com>
2017-10-01 22:26:31 +0800

    auto sign

--- auto.sh
+++ auto.sh
@@ -1,12 +1,12 @@
 
 cat trick
-# if [ $? -eq 0 ]; then
-#   rm -rf trick
-# else
-#   touch trick
-#   echo 'auto sign' > trick
-# fi
-echo 'auto sign' > trick
+if [ $? -eq 0 ]; then
+  # rm -rf trick
+  echo '' > trick
+else
+  echo 'auto sign' > trick
+fi
+# echo 'auto sign' > trick
 git add *
 git ci -m 'auto sign'
 git push origin
