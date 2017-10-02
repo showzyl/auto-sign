862596eda811d4c4e87aaa6c6ad2a0a6d2a8c7b4
showzyl <showzyl520@gmail.com>
2017-10-02 21:18:28 +0800

    auto sign

--- auto.sh
+++ auto.sh
@@ -1,11 +1,11 @@
 
 cat trick
-if [ $? -eq 0 ]; then
-  # rm -rf trick
-  echo '' > trick
-else
-  echo 'auto sign' > trick
-fi
+# if [ $? -eq 0 ]; then
+#   # rm -rf trick
+#   echo '' > trick
+# else
+#   echo 'auto sign' > trick
+# fi
 # echo 'auto sign' > trick
 git add *
 git ci -m 'auto sign'
